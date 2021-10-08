ce00a6f717b8af2489d72c8ce372c9ef76e652d6
JHONATHAN_NICOLAS_M_SILVA <jhonathannicolas@gmail.com>
2021-10-08 01:02:38 -0300

    Adição das clases data e despesas

---
+++ app/data.java
@@ -0,0 +1,40 @@
+package app;
+
+public class Data {
+	
+	/**
+	 * Atributos da classe Data
+	 */
+	int    mes, 
+	       ano,
+	
+	/**
+	 * Metodos construtores
+	 */
+	public Data() {  //metodo construtor padrao
+		
+	}
+	
+	public Data(int mes, int ano) { //construtor alternativo
+		this.mes = mes;
+		this.ano = ano; 
+	}
+	
+	
+	public String getDia() {
+		//TODO
+	}
+
+	public String getAno() {
+		//TODO
+	}
+
+
+    public String setDia() {
+		//TODO
+	}
+
+	public String setAno() {
+		//TODO
+	}
+}
