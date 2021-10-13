c9730aa96012d61d3422ef3ded7a54d303c5c7f4
Genilson Silva De Araújo Junior <61212256+GenilsonJunior99006@users.noreply.github.com>
2021-10-12 23:25:58 -0300

    Adição da Classe Subcategoria
    Adição da Classe Subcategoria

---
+++ src/republica/Subcategoria.java
@@ -0,0 +1,18 @@
+package republica;
+
+public class Subcategoria {
+	    
+	    	String descricaoSubcategoria;
+
+	    public Subcategoria (String descricao) {
+	        this.descricaoSubcategoria = descricao;
+	    }
+
+	    public String getDescricao() { // Deve retornar o valor da metodo descricaoSubcategoria
+	        return descricaoSubcategoria;
+	    }
+
+	    public void setDescricao() { // Deve setar o valor da metodo descricaoSubcategoria
+	      
+	    }
+}
